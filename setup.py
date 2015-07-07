e69f66aee80e1da9528b8676da890c4c7bc02791
maheshakya <pmaheshakya4@gmail.com>
2015-07-07 20:58:53 +0530

    test

--- setup.py
+++ setup.py
@@ -7,22 +7,6 @@ setup(
     version='0.1.2',
     author='Ole Krause-Sparmann',
     author_email='ole@pixelogik.de',
-    packages=[
-        'nearpy',
-        'nearpy.distances',
-        'nearpy.experiments',
-        'nearpy.filters',
-        'nearpy.hashes',
-        'nearpy.storage',
-        'nearpy.utils'
-    ],
-    url='http://pypi.python.org/pypi/NearPy/',
-    license='LICENSE.txt',
-    description='Framework for fast approximated nearest neighbour search.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "redis"
+  
     ],
 )
